aad1d34cc4282e36694a2e10873ffc5a1dc361f1
Stepan <serevussnag@gmail.com>
2019-02-23 17:47:46 +0300

    1

--- core/src/com/dungeonsandorcs/rlgame/DungeonGame.java
+++ core/src/com/dungeonsandorcs/rlgame/DungeonGame.java
@@ -6,21 +6,24 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class DungeonGame extends ApplicationAdapter {
+
+	private static final int FRAME_COLS =  4; // вертикаль
+	 private static final int FRAME_ROWS = 4; // горизонт
+
    private Player player;
    private  OrthographicCamera camera;
-   private Texture walkSheet = new Texture(Gdx.files.internal("/Users/alisavolkova/Documents/AppGame/android/assets/adventurer_f.png"));
-   private TextureRegion[] walkFrames;
-   private SpriteBatch spriteBatch;
-   private Animator animator = new Animator(walkSheet,walkFrames,spriteBatch);
+   private Animator animator = new Animator();
    private SpriteBatch batch;
 
 	@Override
 	public void create () {
 
+		animator.create();
 
 	}
 
@@ -29,7 +32,6 @@ public class DungeonGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		animator.create();
 		animator.render();
 		batch.end();
 
