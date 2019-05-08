a220c476fc11c5c31f5b73a09912e28c3b1be957
Stepan <serevussnag@gmail.com>
2019-05-09 00:04:04 +0300

    5v01.4

---
+++ core/src/com/dungeonsandorcs/rlgame/utils/Objects.java
@@ -0,0 +1,11 @@
+package com.dungeonsandorcs.rlgame.utils;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Objects {
+public SpriteBatch spriteBatch;
+public OrthographicCamera camera;
+public World world;
+}
