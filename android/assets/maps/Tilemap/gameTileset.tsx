5c468cab3378d5b15788336b79c29e68dafeb7f7
Stepan <serevussnag@gmail.com>
2019-05-25 18:52:43 +0300

    7vv

---
+++ android/assets/maps/Tilemap/gameTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.01.30" name="gameTileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1024" columns="32">
+ <image source="../Tilesheet/colored.png" width="543" height="543"/>
+</tileset>
